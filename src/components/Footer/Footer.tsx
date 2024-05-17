de21a6f7ac2531b985e1beb0f7011494e7e7ec95
sweetcter <daylasep0175@gmail.com>
2024-05-17 23:40:39 +0700

    feat: staff card and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className='bg-gradient-to-r from-[#FC9F9F] to-[#9BCBF8] py-3 text-center '>
+            Copyright to ImtaLabs @ {new Date().getFullYear()}
+        </div>
+    );
 };
 
 export default Footer;
