ffbe9771d50bc5f019553d71c3fb93b7acb2a4a7
sweetcter <daylasep0175@gmail.com>
2024-05-18 21:14:12 +0700

    feat: confirmation-form component ui

---
+++ src/components/elements/ConfirmationForm.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { Input } from '../ui/input';
+import { Calendar } from '../ui/calendar';
+import { useId, useState } from 'react';
+import { Button } from '../ui/button';
+import { Label } from '../ui/label';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { format } from 'date-fns';
+import { cn } from '~/lib/utils';
+
+const ConfirmationForm = () => {
+    const [date, setDate] = useState<Date>();
+    const inputId = {
+        fullnameId: useId(),
+        emailId: useId(),
+        phoneNumberId: useId(),
+    };
+    return (
+        <div className=' flex items-center justify-center'>
+            <form className='mb-4 w-[500px]'>
+                <div className='mb-3'>
+                    <Label className='text-base' htmlFor={inputId.fullnameId}>
+                        Fullname
+                    </Label>
+                    <Input placeholder='Enter your full name' id={inputId.fullnameId}></Input>
+                </div>
+                <div className='mb-3'>
+                    <Label className='text-base' htmlFor={inputId.emailId}>
+                        Email
+                    </Label>
+                    <Input placeholder='Enter your email' id={inputId.emailId}></Input>
+                </div>
+
+                <div className='mb-3'>
+                    <Label className='text-base' htmlFor={inputId.phoneNumberId}>
+                        Phone number
+                    </Label>
+                    <div className='flex items-center justify-between gap-3'>
+                        <Input
+                            placeholder='Enter your phone number'
+                            className='md:w-[50%]'
+                            id={inputId.phoneNumberId}
+                        ></Input>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={'outline'}
+                                    type='button'
+                                    className={cn(
+                                        'w-[240px] justify-start text-left font-normal',
+                                        !date && 'text-muted-foreground'
+                                    )}
+                                >
+                                    <CalendarIcon className='mr-2 h-4 w-4' />
+                                    {date ? format(date, 'PPP') : <span>Pick a date</span>}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className='w-auto p-0' align='start'>
+                                <Calendar mode='single' selected={date} onSelect={setDate} initialFocus />
+                            </PopoverContent>
+                        </Popover>
+                    </div>
+                </div>
+                <Button type='submit' className='mt-2 w-full rounded-lg'>
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default ConfirmationForm;
--- src/components/elements/StaffCard.tsx
+++ src/components/elements/StaffCard.tsx
@@ -1,16 +1,15 @@
 const StaffCard = () => {
-    const image = 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg';
     return (
         <div className='px-[2.625rem] py-7'>
             <a href='/'>
                 <div
                     className='bg-cover bg-center bg-no-repeat pt-[100%]'
                     style={{
-                        backgroundImage: `url(${image})`,
+                        backgroundImage: `url(https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg)`,
                     }}
                 ></div>
             </a>
-            <h4 className='bg-[#D9D9D9] py-4 text-center text-lg'>Staffs name</h4>
+            <h4 className='bg-[#D9D9D9] py-4 text-center text-lg'>Staff&apos;s name</h4>
         </div>
     );
 };
