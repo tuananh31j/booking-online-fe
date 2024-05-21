14b4b4ad829abef57e4be4569b6ec3670e74e6c1
sweetcter <daylasep0175@gmail.com>
2024-05-21 15:48:35 +0700

    feat: layout admin and admin sidebar

--- src/app/[locale]/(protected)/dashboard/layout.tsx
+++ src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import AdminSidebar from '~/components/PrivateComponent/AdminSideBar';
+
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div>menu {children}</div>;
+    return (
+        <>
+            <div className='fixed bottom-0 left-0 right-0 top-0 -z-10 bg-[#5e72e4] transition-colors duration-300 dark:bg-[#051139]'></div>
+            <AdminSidebar />
+            <main className='relative h-full max-h-screen rounded-xl transition-all duration-200 ease-in-out xl:ml-[17rem]'>
+                {children}
+            </main>
+        </>
+    );
 };
 
 export default AdminLayout;
