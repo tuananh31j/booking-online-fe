32bb7eb36feb5e3e4ccb8775ef44dba3f18cb27b
QuocDL <145692924+QuocDL@users.noreply.github.com>
2024-05-23 12:08:08 +0700

    feat: maps-api

--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -5,7 +5,7 @@ import SwitchOptions from '~/components/Layouts/Header/_components/SwitchOptions
 const Header = () => {
     return (
         <header className='pt-[15px]'>
-            <div className='flex items-center justify-between lg:items-start'>
+            <div className='mx-6 flex max-w-[1440px] items-center justify-between lg:items-start custom:mx-auto '>
                 <Logo />
                 <Socials />
                 <SwitchOptions />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,23 @@ const config = {
     content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
     prefix: '',
     theme: {
-        container: {
-            center: true,
-            padding: '2rem',
-            screens: {
-                '2xl': '1400px',
-            },
+        screens: {
+            sm: '640px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '768px',
+            // => @media (min-width: 768px) { ... }
+
+            lg: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+
+            custom: '1440px',
+            // => @media (min-width: 1536px) { ... }
         },
         extend: {
             colors: {
