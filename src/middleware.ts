c14dce1f4d5ea1a6377df95b6d1eabcca3807d64
QuangMphann <quangphan2705@gmail.com>
2024-05-23 15:38:46 +0700

    fix: locale prefix in pathname removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,20 @@ export default createMiddleware({
 
     // Used when no locale matches
     defaultLocale: 'en',
+
+    localePrefix: 'never',
 });
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ['/', '/(vn|en)/:path*'],
+    // matcher: ['/', '/(vn|en)/:path*'],
+
+    matcher: [
+        // Match all pathnames except for
+        // - … if they start with `/api`, `/_next` or `/_vercel`
+        // - … the ones containing a dot (e.g. `favicon.ico`)
+        '/((?!api|_next|_vercel|.*\\..*).*)',
+        // However, match all pathnames within `/users`, optionally with a locale prefix
+        '/([\\w-]+)?/users/(.+)',
+    ],
 };
