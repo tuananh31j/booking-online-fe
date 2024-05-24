496123a8b2e55a3c0e3993994bd8c3ccedff1f4b
Nguyen Tuan Anh <tuananh31j@gmail.com>
2024-05-24 21:13:48 +0700

    feat: action link component

--- src/components/elements/ModeToggle.tsx
+++ src/components/elements/ModeToggle.tsx
@@ -12,6 +12,7 @@ import {
     DropdownMenuTrigger,
 } from '~/components/ui/dropdown-menu';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 export function ModeToggle({ className }: { className?: string }) {
     const { setTheme } = useTheme();
@@ -27,7 +28,9 @@ export function ModeToggle({ className }: { className?: string }) {
             <DropdownMenuContent align='end'>
                 <DropdownMenuItem onClick={() => setTheme('light')}>{t('light')}</DropdownMenuItem>
                 <DropdownMenuItem onClick={() => setTheme('dark')}>{t('dark')}</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('system')}>{t('system')}</DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href={'/dashboard'}>{t('system')}</Link>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
