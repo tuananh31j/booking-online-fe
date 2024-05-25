b8ae43b1d0b49f7484145e1c36830b770ffb7455
QuocDL <145692924+QuocDL@users.noreply.github.com>
2024-05-25 08:04:57 +0700

    fix: loading ui

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import LoadingButton from '~/components/elements/LoadingButton';
 
 const loading = () => {
-    return <div>loading...2</div>;
+    return (
+        <div className='flex min-h-[100vh] min-w-[100vw] items-center justify-center'>
+            <LoadingButton className='h-[220px] w-[220px]' />
+        </div>
+    );
 };
 
 export default loading;
