c84c522a36697e3d5894f24a096e376f7853f00f
Tuan Anh <tuananh31j@gmail.com>
2024-06-11 17:11:55 +0700

    feat: list staff admin

--- src/components/_common/TableDisplay/Rows/Staff/UserRow.tsx
+++ src/components/_common/TableDisplay/Rows/Staff/UserRow.tsx
@@ -1,36 +1,37 @@
 import Image from 'next/image';
 import { FC } from 'react';
 import PopupModal from '~/components/_common/PopupModal';
-import StatusTag from '~/components/_common/StatusTag';
-import { UserRole } from '~/constants/enums';
 import { disPlayRoleName } from '~/lib/utils';
 import FormStaff from './FormStaff';
 
 type IUserRowProps = {
-    avt: string;
+    image: string | null;
     name: string;
     email: string;
-    role: UserRole;
-    status: boolean;
+    role: number;
+    phone: string | null;
+    address: string | null;
     createAt: string;
 };
 
-const USER_COLUMN_NAMES = ['Người dùng', 'Chức vụ', 'Trạng thái', 'Ngày tạo', 'Tùy chọn'];
+const USER_COLUMN_NAMES = ['Người dùng', 'Chức vụ', 'Điện thoại', 'Ngày tạo', 'Tùy chọn'];
 
-const UserRow: FC<IUserRowProps> = ({ avt, name, email, role, status, createAt }) => {
+const UserRow: FC<IUserRowProps> = ({ image, name, email, role, phone, createAt }) => {
     const roleName = disPlayRoleName(role);
     return (
         <tr>
             <td className='whitespace-nowrap border-b bg-transparent p-2 align-middle capitalize shadow-transparent dark:border-white/40'>
                 <div className='flex px-2 py-1'>
                     <div>
-                        <Image
-                            src={avt}
-                            alt=''
-                            width={1150}
-                            height={5150}
-                            className='mr-4 inline-flex h-9 w-9 items-center justify-center rounded-xl text-sm text-white transition-all duration-200 ease-in-out'
-                        />
+                        {image && (
+                            <Image
+                                src={image}
+                                alt=''
+                                width={1150}
+                                height={5150}
+                                className='mr-4 inline-flex h-9 w-9 items-center justify-center rounded-xl text-sm text-white transition-all duration-200 ease-in-out'
+                            />
+                        )}
                     </div>
                     <div className='flex flex-col justify-center'>
                         <h6 className='mb-0 text-sm capitalize leading-normal dark:text-white'>{name}</h6>
@@ -46,7 +47,7 @@ const UserRow: FC<IUserRowProps> = ({ avt, name, email, role, status, createAt }
                 </p>
             </td>
             <td className='whitespace-nowrap border-b bg-transparent p-2 align-middle text-sm capitalize leading-normal shadow-transparent dark:border-white/40'>
-                <StatusTag status={status} />
+                {phone}
             </td>
             <td className='whitespace-nowrap border-b bg-transparent p-2 align-middle capitalize shadow-transparent dark:border-white/40'>
                 <span className='text-xs font-semibold capitalize leading-tight text-slate-400 dark:text-white dark:opacity-80'>
--- src/pages/Admins/StaffManagement/index.tsx
+++ src/pages/Admins/StaffManagement/index.tsx
@@ -1,108 +1,28 @@
-import React from 'react';
+'use client';
+
 import { USER_COLUMN_NAMES, UserRow } from '~/components/_common/TableDisplay/Rows';
 import FormStaff from '~/components/_common/TableDisplay/Rows/Staff/FormStaff';
 import TableDisplay from '~/components/_common/TableDisplay/TableDisplay';
-import { UserRole } from '~/constants/enums';
-
-const dataFake = [
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        role: UserRole.Admin,
-        status: true,
-        createAt: '2022-01-01',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Jane Smith',
-        email: 'jane.smith@example.com',
-        role: UserRole.Staff,
-        status: false,
-        createAt: '2022-02-15',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Alice Johnson',
-        email: 'alice.johnson@example.com',
-        role: UserRole.Admin,
-        status: true,
-        createAt: '2022-03-20',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Bob Brown',
-        email: 'bob.brown@example.com',
-        role: UserRole.Staff,
-        status: false,
-        createAt: '2022-04-10',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Eve White',
-        email: 'eve.white@example.com',
-        role: UserRole.Admin,
-        status: true,
-        createAt: '2022-05-05',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Michael Green',
-        email: 'michael.green@example.com',
-        role: UserRole.Admin,
-        status: false,
-        createAt: '2022-06-30',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Sarah Black',
-        email: 'sarah.black@example.com',
-        role: UserRole.Staff,
-        status: true,
-        createAt: '2022-07-25',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'David Gray',
-        email: 'david.gray@example.com',
-        role: UserRole.Staff,
-        status: false,
-        createAt: '2022-08-12',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'Olivia Brown',
-        email: 'olivia.brown@example.com',
-        role: UserRole.Admin,
-        status: true,
-        createAt: '2022-09-18',
-    },
-    {
-        avt: 'https://i.pinimg.com/564x/4e/79/d3/4e79d37bd5fe58e4f9630ee21a61f4a8.jpg',
-        name: 'William Smith',
-        email: 'william.smith@example.com',
-        role: UserRole.Staff,
-        status: false,
-        createAt: '2022-10-22',
-    },
-];
+import { useListStaffClientQuery } from '~/store/services/staff.service';
 
 const StaffListManager = () => {
+    const { data } = useListStaffClientQuery();
     return (
         <TableDisplay
             title='Danh sách nhân viên'
             columnNames={USER_COLUMN_NAMES}
             action={{ element: FormStaff, modalTitle: 'Thêm mới nhân viên' }}
         >
-            {dataFake.map((item, i) => (
+            {data?.data.data.map((item, i) => (
                 <UserRow
                     key={i}
-                    avt={item.avt}
+                    image={item.image}
                     name={item.name}
                     email={item.email}
                     role={item.role}
-                    status={item.status}
-                    createAt={item.createAt}
+                    phone={item.phone}
+                    address={item.address}
+                    createAt={item.created_at}
                 />
             ))}
         </TableDisplay>
