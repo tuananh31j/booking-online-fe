1ec13196918e965aed5092cf6ddf66cef4a24254
Tuan Anh <tuananh31j@gmail.com>
2024-06-11 10:37:35 +0000

    chore: change file name

--- src/types/Service.ts
+++ src/types/Service.ts
@@ -12,3 +12,31 @@ export type IService = {
 export type IGetServiceResponse = {
     data: IService[];
 };
+export type IServiceBody = {
+    name: string;
+    categorie_id: number;
+    price: string;
+    describe: string;
+};
+
+export type IServiceResponse = {
+    data: {
+        id: number;
+        name: string;
+        categorie_id: number;
+        describe: string;
+        price: string;
+        created_at: string;
+        updated_at: string;
+    };
+};
+
+export interface IServiceItem {
+    id: number;
+    name: string;
+    categorie_id: number;
+    describe: string;
+    price: string;
+    created_at: string;
+    updated_at: string;
+}
