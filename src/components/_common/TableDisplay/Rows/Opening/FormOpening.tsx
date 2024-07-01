266350cc651f31d1faff061c4dceefab7b2df1d1
QuocDL <145692924+QuocDL@users.noreply.github.com>
2024-07-01 07:09:10 +0700

    refactor: calendar opening hour

--- src/components/_common/TableDisplay/Rows/Opening/FormOpening.tsx
+++ src/components/_common/TableDisplay/Rows/Opening/FormOpening.tsx
@@ -1,16 +1,23 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { format } from 'date-fns';
+import { CalendarIcon, HelpCircle } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import FormItemDisplay from '~/components/_common/FormItemDisplay';
-import { Form, FormField, FormMessage } from '~/components/ui/form';
+import { Button } from '~/components/ui/button';
+import { Calendar } from '~/components/ui/calendar';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
 import useToastDisplay from '~/hooks/useToastDisplay';
-import { useCreateOpeningMutation } from '~/store/services/opening.service';
-import { useGetListStoreQuery } from '~/store/services/store.service';
+import { cn } from '~/lib/utils';
+import { useCreateOpeningMutation, useGetOpeningDetailQuery } from '~/store/services/opening.service';
+import { ErrorOpeningHours, isMessageError, isOpeningHourError } from '~/types/Error/Helper/Store';
 
 const FormOpeningSchema = z.object({
-    day: z.string({ required_error: 'Vui lòng chọn ngày!' }),
+    day: z.date({ required_error: 'Vui lòng chọn ngày!' }),
     opening_time: z.string({ required_error: 'Vui lòng chọn giờ bắt đầu!' }),
     closing_time: z.string({ required_error: 'Vui lòng chọn giờ kết thúc!' }),
 });
@@ -20,21 +27,20 @@ type IFormOpening = z.infer<typeof FormOpeningSchema>;
 const FormOpening = ({ onCloseModal, id }: { onCloseModal: () => void; id: number }) => {
     const toast = useToastDisplay();
     const [createOpening, createOpeningState] = useCreateOpeningMutation();
-    const { data: storeList, isLoading: isStoreLoading } = useGetListStoreQuery();
-
+    const { data: OpeningData, isError } = useGetOpeningDetailQuery(id, { skip: !id });
+    const opening = OpeningData?.data.data;
     const form = useForm<IFormOpening>({ resolver: zodResolver(FormOpeningSchema) });
-    const idStore = storeList?.data.data.find((store) => store.id === id)?.id;
-
+    const [dateValid, setValidDay] = useState<Date[] | null>();
     const onSubmit: SubmitHandler<IFormOpening> = async (data) => {
         await new Promise((resolve) => {
             resolve(data);
         });
         try {
             const appendSeconds = (time: string) => (time.includes(':') ? `${time}:00` : `${time}:00:00`);
-
+            const dayFormat = format(data.day, 'yyyy-MM-dd');
             const openingHours = [
                 {
-                    day: data.day,
+                    day: dayFormat,
                     opening_time: appendSeconds(data.opening_time),
                     closing_time: appendSeconds(data.closing_time),
                 },
@@ -46,14 +52,32 @@ const FormOpening = ({ onCloseModal, id }: { onCloseModal: () => void; id: numbe
                     opening_hours: openingHours,
                 },
             }).unwrap();
-            if (res.data) {
-                toast({ title: 'Thêm mới thành công', status: 'success' });
-                onCloseModal();
-            }
+            toast({ title: 'Thêm mới thành công', status: 'success' });
+            onCloseModal();
         } catch (error) {
-            toast({ title: 'Thêm mới thất bại', status: 'destructive' });
+            if (isOpeningHourError(error)) {
+                const objectKeys = Object.keys(error.data.error) as ErrorOpeningHours[];
+                objectKeys.forEach((key: ErrorOpeningHours) => {
+                    const errorMessage = error.data.error[key].join(', ');
+                    form.setError(key, { message: errorMessage });
+                });
+            }
+            if (isMessageError(error)) {
+                toast({ title: `${error.data.message}`, status: 'destructive' });
+            }
         }
     };
+    useEffect(() => {
+        const disableDay = opening?.map((item) => new Date(item.day));
+        if (disableDay) {
+            setValidDay(disableDay);
+        } else {
+            setValidDay(null);
+        }
+        if (isError) {
+            setValidDay(null);
+        }
+    }, [OpeningData]);
     return (
         <div className='mx-auto flex flex-col justify-center'>
             {
@@ -66,13 +90,55 @@ const FormOpening = ({ onCloseModal, id }: { onCloseModal: () => void; id: numbe
                                 render={({ field }) => {
                                     // console.log(field);
                                     return (
-                                        <FormItemDisplay
-                                            title='Ngày mở cửa '
-                                            placeholder='Nhập Ngày bắt đầu mở cửa !'
-                                            {...field}
-                                            require
-                                            type='date'
-                                        />
+                                        <FormItem className='flex flex-col'>
+                                            <FormLabel className='flex justify-between'>
+                                                <span>
+                                                    Ngày mở cửa: <span className='text-[#e41a0f]'>*</span>
+                                                </span>
+                                            </FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={'outline'}
+                                                            className={cn(
+                                                                'w-[240px] pl-3 text-left font-normal',
+                                                                !field.value && 'text-muted-foreground'
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, 'PPP')
+                                                            ) : (
+                                                                <span>Chọn ngày mở cửa</span>
+                                                            )}
+                                                            <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className='w-auto p-0' align='start'>
+                                                    <Calendar
+                                                        mode='single'
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) => {
+                                                            if (!dateValid) {
+                                                                return false;
+                                                            }
+                                                            console.log(dateValid);
+                                                            return dateValid.some((validDate) => {
+                                                                return (
+                                                                    validDate.getFullYear() === date.getFullYear() &&
+                                                                    validDate.getMonth() === date.getMonth() &&
+                                                                    validDate.getDate() === date.getDate()
+                                                                );
+                                                            });
+                                                        }}
+                                                        initialFocus
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+
+                                            <FormMessage />
+                                        </FormItem>
                                     );
                                 }}
                             />
@@ -111,7 +177,10 @@ const FormOpening = ({ onCloseModal, id }: { onCloseModal: () => void; id: numbe
                             />
                             <FormMessage></FormMessage>
                         </div>
-                        <button className='mt-3 flex h-14 w-full flex-col items-center justify-center rounded-md border-transparent bg-card p-3 text-foreground'>
+                        <button
+                            disabled={createOpeningState.isLoading}
+                            className='mt-3 flex h-14 w-full flex-col items-center justify-center rounded-md border-transparent bg-card p-3 text-foreground'
+                        >
                             Submit
                         </button>
                     </form>
